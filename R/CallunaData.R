0371e829762271a8d44637195b1a97a551de8035
Robin Boyd <robboy@ceh.ac.uk>
2023-01-09 16:06:28 +0000

    document e.g. data

---
+++ R/CallunaData.R
@@ -0,0 +1,17 @@
+#' Data for the Calluna vulgaris example in Boyd et al. (2023).
+#'
+#' Data on sample membership {1,0} and occurrence of Calluna vulgaris {1,0} in Briatin.
+#' Each row pertains to one 1 km grid square (OSGB 1936). The identity of the 1 km grid square is not provided.
+#'
+#' @docType data
+#'
+#' @usage data(Calluna)
+#'
+#' @format An object of class \code{"data.frame"}.
+#'
+#' @keywords datasets
+#'
+#' @references Carey, P.., Wallis, S., Chamberlin, P.M., Copper, A., Maskell, L., McCann, T., Murphy, J., Norton, L., Reynolds, B., Scott, W., Simpson, I., Smart, S., Ullyett, J., 2008. Countryside Survey: UK Results from 2007 CHAPTER 1 Countryside Survey Methodology Secretary of State for Environment, Food and Rural Aff airs.
+#'Morton, D., Rowland, C., Wood, C., Meek, L., Marston, C., Smith, G., 2014. Land Cover Map 2007 (1km percentage target class, GB) v1.2. https://doi.org/https://doi.org/10.5285/fdf8c8d3-5998-45a5-8431-7f5e6302fc32
+#'Stroh, P.A., Walker, K., Humphrey, T.A., Pescott, O.L., Burkmar, R., 2023. Plant Atlas 2020: Mapping Changes in the Distribution of the British and Irish Flora. Princeton Univ. Press.
+
